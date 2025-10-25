46be5dbe2dbbf07fa2c45384652c9983105c46c1
Jonathan <Jonathanallyson601@gmail.com>
2025-10-24 21:39:37 -0300

    Update home.page.html

--- paginas/js.js
+++ paginas/js.js
@@ -1,12 +1,12 @@
 // Config Firebase
 const firebaseConfig = {
-  apiKey: "AIzaSyAbVJUTwovjKHua6OWP_yIncKYytEEwPfo",
-  authDomain: "tcc-994f7.firebaseapp.com",
-  projectId: "tcc-994f7",
-  storageBucket: "tcc-994f7.firebasestorage.app",
-  messagingSenderId: "848443566233",
-  appId: "1:848443566233:web:b9be3b2beccb027fa53008",
-  measurementId: "G-SGB2LYMT17"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: ""
 };
 
 // Inicializa Firebase
